2384445c6632eda41efa0a8f7cb0a555e5ece045
Wilson Matokanovic Jubior <wilsonmjuniorx@gmail.com>
2025-09-22 19:12:34 -0300

    feat: create shared hooks

---
+++ src/hooks/useFavoriteMovie.ts
@@ -0,0 +1,75 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useCallback, useMemo } from 'react';
+import Toast from 'react-native-toast-message';
+
+import { queryClient } from '@/api/config';
+import { useMarkAsFavoriteMovie, useUnmarkAsFavoriteMovie } from '@/api/mutations';
+import { isFavoriteMoviesKey } from '@/api/queries';
+import { MovieSchemaType } from '@/models/MovieSchema';
+
+export function useFavoriteMovie(isFavorite: boolean) {
+  const { mutateAsync: markAsFavorite } = useMarkAsFavoriteMovie({
+    onSuccess: async (data, movie, ...restProps) => {
+      await queryClient.invalidateQueries({
+        queryKey: [isFavoriteMoviesKey, movie.movieId],
+      });
+
+      Toast.show({
+        type: 'success',
+        text1: 'Filme marcado como favorito',
+      });
+      return { data, movie, ...restProps };
+    },
+    onError: () => {
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao marcar filme como favorito',
+      });
+    },
+  });
+  const { mutateAsync: unmarkAsFavorite } = useUnmarkAsFavoriteMovie({
+    onSuccess: async (data, movieId, ...restProps) => {
+      await queryClient.invalidateQueries({
+        queryKey: [isFavoriteMoviesKey, movieId],
+      });
+
+      Toast.show({
+        type: 'success',
+        text1: 'Filme removido da lista de favoritos',
+      });
+
+      return { data, movieId, ...restProps };
+    },
+    onError: () => {
+      Toast.show({
+        type: 'error',
+        text1: 'Erro ao remover filme da lista de favoritos',
+      });
+    },
+  });
+
+  const handleFavorite = useCallback(
+    (movie: MovieSchemaType) => {
+      if (isFavorite) {
+        unmarkAsFavorite(movie.id);
+      } else {
+        markAsFavorite({
+          movieId: movie.id,
+          title: movie.title,
+          posterPath: movie.poster_path ?? '',
+          backdropPath: movie.backdrop_path ?? '',
+        });
+      }
+    },
+    [isFavorite, markAsFavorite, unmarkAsFavorite]
+  );
+
+  const favoriteIcon = useMemo((): keyof typeof MaterialCommunityIcons.glyphMap => {
+    return isFavorite ? 'bookmark' : 'bookmark-outline';
+  }, [isFavorite]);
+
+  return {
+    favoriteIcon,
+    handleFavorite,
+  };
+}
